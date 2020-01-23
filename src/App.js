ceacf00979ac468d5314063b56a44d4be528d7cd
Jake Thomas <jmaxt12@gmail.com>
2020-01-23 15:03:57 -0700

    created character builder coomponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import CharacterBuilder from '../src/container/CharacterBuilder';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+      
+      <CharacterBuilder />
 
     </div>
   );
---
+++ src/container/CharacterBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CharacterBuilder extends Component {
+    state = {
+
+    }
+    render() { 
+        return ( 
+            <h1>Character Builder</h1>
+         );
+    }
+}
+ 
+export default CharacterBuilder;
