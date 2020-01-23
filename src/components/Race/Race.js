6598777badb2c2e0b8f9d2eb4621c2788fc99cf7
Jake Thomas <jmaxt12@gmail.com>
2020-01-23 15:08:01 -0700

    added race component

---
+++ src/components/Race/Race.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Race = (props) => {
+    return ( 
+        <h3>Choose Your Race:</h3>
+     );
+}
+ 
+export default Race;
--- src/container/CharacterBuilder.js
+++ src/container/CharacterBuilder.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+import Race from '../components/Race/Race';
+
 class CharacterBuilder extends Component {
     state = {
 
     }
     render() { 
         return ( 
-            <h1>Character Builder</h1>
+            <div>
+                <h1>Character Builder</h1>
+                <Race />
+            </div>
          );
     }
 }
